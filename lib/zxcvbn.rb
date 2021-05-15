ec90ff6eaac956a0c41fc40d0c32a7a3f0f60c2f
Rafael Santos <formigarafa@gmail.com>
2021-05-16 00:48:38 +1200

    blank gem

---
+++ lib/zxcvbn.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "zxcvbn/version"
+
+module Zxcvbn
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/zxcvbn/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Zxcvbn
+  VERSION = "0.1.0"
+end
