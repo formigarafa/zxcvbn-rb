9a2fc1ede6caffc6187d5515bb3df0b57f9037d3
Rafael Santos <formigarafa@gmail.com>
2021-05-16 20:59:35 +1200

    cache js results to speedup tests

--- spec/support/js_helpers.rb
+++ spec/support/js_helpers.rb
@@ -1,4 +1,6 @@
 require 'mini_racer'
+require 'json'
+require 'digest'
 
 module JsHelpers
   def js_ctx
@@ -15,6 +17,21 @@ module JsHelpers
   end
 
   def js_matcher(matcher, password)
-    js_ctx.eval(%Q{matching['#{matcher}']("#{password.gsub('"', '\"')}")})
+    cached_eval(%Q{matching['#{matcher}']("#{password.gsub('"', '\"')}")})
+  end
+
+  def cached_eval(js_str)
+    tmp_hash = Digest::MD5.base64digest js_str.to_s
+    cache_name = Pathname(File.expand_path("../../../tmp/#{tmp_hash}.json", __FILE__))
+    if File.exists?(cache_name)
+      JSON.parse(File.read(cache_name))
+    else
+      uncached_result = js_ctx.eval(js_str)
+      if !File.exists?(Pathname(File.expand_path("../../../tmp", __FILE__)))
+        Dir.mkdir Pathname(File.expand_path("../../../tmp", __FILE__))
+      end
+      File.write(cache_name, uncached_result)
+      uncached_result
+    end
   end
 end
