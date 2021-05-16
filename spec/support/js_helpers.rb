b3be772bcb0354eab735004caf33a746310c2bfb
Rafael Santos <formigarafa@gmail.com>
2021-05-16 21:15:23 +1200

    improve/fix js result cache

--- spec/support/js_helpers.rb
+++ spec/support/js_helpers.rb
@@ -1,6 +1,7 @@
 require 'mini_racer'
 require 'json'
 require 'digest'
+require 'base64'
 
 module JsHelpers
   def js_ctx
@@ -21,16 +22,16 @@ module JsHelpers
   end
 
   def cached_eval(js_str)
-    tmp_hash = Digest::MD5.base64digest js_str.to_s
+    tmp_hash = Base64.urlsafe_encode64 Digest::MD5.digest(js_str.to_s), padding: false
     cache_name = Pathname(File.expand_path("../../../tmp/#{tmp_hash}.json", __FILE__))
     if File.exists?(cache_name)
       JSON.parse(File.read(cache_name))
     else
       uncached_result = js_ctx.eval(js_str)
-      if !File.exists?(Pathname(File.expand_path("../../../tmp", __FILE__)))
-        Dir.mkdir Pathname(File.expand_path("../../../tmp", __FILE__))
+      if !File.exists?(Pathname(File.expand_path("..", cache_name)))
+        Dir.mkdir Pathname(File.expand_path("..", cache_name))
       end
-      File.write(cache_name, uncached_result)
+      File.write(cache_name, uncached_result.to_json)
       uncached_result
     end
   end
